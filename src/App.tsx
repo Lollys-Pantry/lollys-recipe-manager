4a11d0bc42484e289eddceb5e9a7f6f4f3cdfef1
Lea Ann Bradford <leaann@notch8.com>
2022-08-06 17:16:02 -0700

    add typescript to project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './img/LP_Retro_Logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.lollyspantry.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Lolly's Pantry
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
