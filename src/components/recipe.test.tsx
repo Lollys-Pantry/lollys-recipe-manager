05b6d54caef6af901f6928f823e95b8ee9d668c3
Lea Ann Bradford <leaann@notch8.com>
2022-11-13 12:52:19 -0800

    prettier and jest

--- src/components/recipe.test.tsx
+++ src/components/recipe.test.tsx
@@ -7,11 +7,6 @@ const recipeItem = {
   id: 1,
   name: "Recipe Name",
   description: "Recipe description",
-  servings: 10,
-  prep_time: "10 minutes",
-  cook_time: "20 minutes",
-  sprouty_pie: false,
-  lollys_pantry: true,
 };
 
 describe("when rendered", () => {
@@ -22,21 +17,10 @@ describe("when rendered", () => {
           id={recipeItem.id}
           name={recipeItem.name}
           description={recipeItem.description}
-          servings={recipeItem.servings}
-          prep_time={recipeItem.prep_time}
-          cook_time={recipeItem.cook_time}
-          sprouty_pie={recipeItem.sprouty_pie}
-          lollys_pantry={recipeItem.lollys_pantry}
         />
       </Router>
     );
     expect(screen.getByText("Recipe Name")).toBeInTheDocument();
     expect(screen.getByText("Recipe description")).toBeInTheDocument();
-    expect(screen.getByText("Servings: 10")).toBeInTheDocument();
-    expect(screen.getByText("Prep Time: 10 minutes")).toBeInTheDocument();
-    expect(screen.getByText("Cooking Time: 20 minutes")).toBeInTheDocument();
-    expect(
-      screen.getByText("Official Lolly's Pantry recipe")
-    ).toBeInTheDocument();
   });
 });
--- src/components/recipe.tsx
+++ src/components/recipe.tsx
@@ -6,28 +6,29 @@ import { RecipeItem } from "../types/data";
 import "./recipe.css";
 
 function Recipe(props: RecipeItem) {
-  const {
-    id,
-    name,
-    description,
-  } = props;
+  const { id, name, description } = props;
 
   const navigate = useNavigate();
 
   return (
-    
-        <tr>
-          <td><strong>{name}</strong><br />{description}</td>
-          <td><Button
-                variant="outline-info"
-                size="sm"
-                onClick={() => {
-                  navigate(`/recipe/${id}`);
-                }}
-      >
-        Edit Recipe
-      </Button></td>
-        </tr>
+    <tr>
+      <td>
+        <strong>{name}</strong>
+        <br />
+        {description}
+      </td>
+      <td>
+        <Button
+          variant="outline-info"
+          size="sm"
+          onClick={() => {
+            navigate(`/recipe/${id}`);
+          }}
+        >
+          Edit Recipe
+        </Button>
+      </td>
+    </tr>
   );
 }
 
